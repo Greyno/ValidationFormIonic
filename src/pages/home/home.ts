9055d29cb6843059bf60425e365aa772b72a30dd
dries11 <daniel.ries@ymail.com>
2017-02-28 16:21:32 -0500

    Added ionic side to repo

---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  private success : any;
+  private serverURL : "";
+
+  private loginForm: any = {
+    formUsername: "",
+    formPassword: ""    
+  }
+  constructor(public navCtrl: NavController, private http: Http) {
+    
+  }
+
+  didSubmit(){
+    console.log(this.loginForm);
+    this.http.post(this.serverURL,this.loginForm).subscribe(result=>{
+      this.responseHolder(result,result);
+    })
+  }
+
+  responseHolder(status, response){
+    if(response.error){
+      console.log("error " + response.error.message);
+    }
+    else{
+      this.success = response;
+    }
+  }
+
+}
